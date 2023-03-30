2bdd386a2f3be844beb28bad65cde294177b10e3
odaney baker <bakerobtb@gmail.com>
2023-03-30 06:52:37 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(driversArray) {
+  return driversArray.slice(0,2);
+}
+
+function returnLastTwoDrivers(driversArray) {
+  return driversArray.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplierValue) {
+  return function(value) {
+    return value * multiplierValue;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driversArray, selectionFunction) {
+  return selectionFunction(driversArray);
+}
